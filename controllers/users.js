73ffbc121ad2d28fa71f7fc0bde07270813bcba4
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-09-17 21:57:03 -0400

    gg

--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,10 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     try {
-        const result = await mongodb.getDb().db('sample_mflix').collection('users').find();
-        result.toArray().then((lists) => {
+        const result = await mongodb.getDb().db().collection('users').find();
+        result.toArray().then((users) => {
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
+            res.status(200).json(users);
         });
     } catch (error) {
         res.status(500).json(error);
@@ -16,10 +16,10 @@ const getAll = async (req, res) => {
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     try {
-        const result = await mongodb.getDb().db('sample_mflix').collection('users').find({ _id: userId });
-        result.toArray().then((lists) => {
+        const result = await mongodb.getDb().db().collection('users').find({ _id: userId });
+        result.toArray().then((users) => {
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
+            res.status(200).json(users[0]);
         });
     } catch (error) {
         res.status(500).json(error);
