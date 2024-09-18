f2ff437829342014e92db0bbd59186f4760a19fb
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-09-17 20:47:24 -0400

    ggs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const mongodb = require('./data/database');
 router.get('/', (req, res) => {res.send('Hello World!')});
 
+router.use('./users', require('./users'));
+
 mongodb.initDb((err) => {
     if (!err) {
         console.log('Database initialized');
