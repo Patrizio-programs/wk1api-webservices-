45d7f08227146fb03789d1e4071cd7c6be374eae
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-09-19 23:18:07 -0400

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,23 @@
 const mongodb = require('../routes/data/database.js')
 const ObjectId = require('mongodb').ObjectId;
-
+const { getDatabase } = require('../routes/data/database.js')
 const getAll = async (req, res) => {
     try {
-        const db = mongodb.getDb();
-        console.log('Database connection:', db);
-        const result = await mongodb.getDb().db().collection('contacts').find();
-        result.toArray().then((contacts) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(contacts);
-        });
+      const result = await mongodb.getDatabase().db().collection('contacts').find()
+      result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+      });
     } catch (error) {
-        res.status(500).json(error);
+      console.error(error);
+      res.status(500).json({ message: 'Error fetching users',  error: error.message });
     }
-}
+  }
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     try {
-        const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+        const result = await mongodb.getDatabase().db().collection('contacts').find({ _id:userId });
         result.toArray().then((contacts) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(contacts[0]);
